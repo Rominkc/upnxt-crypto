4074b9035e05d73f3ae52553b655e0cdad4e89a2
Romin K <37375864+Rominkc@users.noreply.github.com>
2022-01-25 09:50:39 -0500

    Fix issue with crypto rankings page not loading

--- src/components/CryptoTable.js
+++ src/components/CryptoTable.js
@@ -35,15 +35,15 @@ const CryptoTable= props => (
      {props.coinData.map((coinD,index) => (
          <CoinDataTr key={index +1}
                      id={index +1}
-                     price={coinD.DISPLAY.USD.PRICE}
+                     price={coinD.DISPLAY !== undefined ? coinD.DISPLAY.USD.PRICE:""}
                      coinImage={'https://www.cryptocompare.com'+coinD.CoinInfo.ImageUrl}
                      coinLink={'https://www.cryptocompare.com'+coinD.CoinInfo.Url} 
                      coinName={coinD.CoinInfo.FullName}
                      coinId={coinD.CoinInfo.Name}
-                     marketCap={coinD.DISPLAY.USD.MKTCAP} 
-                     totalVol24h={coinD.DISPLAY.USD.VOLUME24HOURTO}  
-                     usdChange24h={coinD.DISPLAY.USD.CHANGE24HOUR}  
-                     percentChange24h={coinD.DISPLAY.USD.CHANGEPCT24HOUR} 
+                     marketCap={coinD.DISPLAY !== undefined ? coinD.DISPLAY.USD.MKTCAP:""} 
+                     totalVol24h={coinD.DISPLAY !== undefined ? coinD.DISPLAY.USD.VOLUME24HOURTO:""}  
+                     usdChange24h={coinD.DISPLAY !== undefined ? coinD.DISPLAY.USD.CHANGE24HOUR:""}  
+                     percentChange24h={coinD.DISPLAY !== undefined ? coinD.DISPLAY.USD.CHANGEPCT24HOUR:""} 
                      />
      ))}
      </tbody> 
